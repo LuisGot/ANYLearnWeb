159792c719c47536ddf3f7be0e2c83064245f9e4
Luis <luisgoettler@gmail.com>
2024-12-26 15:03:37 +0100

    add daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,22 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#ed9b40",
+          secondary: "#f3f4f6",
+          accent: "#ff00ff",
+          neutral: "#1c1c1c",
+          "base-100": "#121212",
+          info: "#67e8f9",
+          success: "#4ade80",
+          warning: "#fde047",
+          error: "#ef4444",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
