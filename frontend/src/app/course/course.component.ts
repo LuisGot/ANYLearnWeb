ab42871efe38c1e5dec781763203449531d5d920
Luis <luisgoettler@gmail.com>
2024-12-28 00:58:38 +0100

    Add back to startpage button

--- frontend/src/app/course/course.component.ts
+++ frontend/src/app/course/course.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { CourseService } from '../shared/course.service';
 import { MarkdownComponent } from 'ngx-markdown';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-course',
-  imports: [MarkdownComponent],
+  imports: [MarkdownComponent, RouterLink],
   templateUrl: './course.component.html',
 })
 export class CourseComponent {
