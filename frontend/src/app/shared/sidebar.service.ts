6101bdd8766918d42c1236e9950399651bc05c39
Luis <luisgoettler@gmail.com>
2025-01-17 17:39:50 +0100

    Add sidebar state management

--- frontend/src/app/shared/sidebar.service.ts
+++ frontend/src/app/shared/sidebar.service.ts
@@ -1,20 +1,38 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { inject, Injectable, PLATFORM_ID, signal } from '@angular/core';
 import { CourseService } from './course.service';
 import { Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({ providedIn: 'root' })
 export class SidebarService {
+  platformId = inject(PLATFORM_ID);
   courseService = inject(CourseService);
   private router = inject(Router);
 
-  isOpen = signal(true);
+  isOpen = signal(this.SidebarState());
+
+  private SidebarState(): boolean {
+    if (this.isBrowser()) {
+      const savedState = sessionStorage.getItem('sidebarOpen');
+      return savedState === null ? true : savedState === 'true';
+    }
+    return false;
+  }
 
   toggleSidebar() {
-    this.isOpen.set(!this.isOpen());
+    const newState = !this.isOpen();
+    this.isOpen.set(newState);
+    if (this.isBrowser()) {
+      sessionStorage.setItem('sidebarOpen', newState.toString());
+    }
   }
 
   learnNew() {
     this.courseService.selectedCourseId.set(null);
     this.router.navigate(['']);
+  }
+
+  isBrowser(): boolean {
+    return isPlatformBrowser(this.platformId);
   }
 }
